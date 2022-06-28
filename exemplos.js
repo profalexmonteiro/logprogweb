4e516308eb314e8297d84bbc3b035a7358d23be7
Alex Monteiro <alex.monteiro@ifam.edu.br>
2022-06-28 11:46:40 -0500

    questao01

--- exemplos.js
+++ exemplos.js
@@ -9,3 +9,18 @@ function calculaArea2(){
     //saida
     document.getElementById("area").value = area;              
 }
+
+
+function somar(){
+    //recuperação de entradas de dados
+    var num1    = document.getElementById("numero1").value;
+    var num2    = document.getElementById("numero2").value;
+    var num3    = document.getElementById("numero3").value;
+    var num4    = document.getElementById("numero4").value;
+    
+    //processamento
+    var soma = numero1 + numero2 + numero3 + numero4;
+
+    //saida
+    document.getElementById("soma").value = soma;
+}
