28193787904d09de915215e6cada85a752fd9d3f
Alex Monteiro <alex.monteiro@ifam.edu.br>
2022-07-05 11:38:13 -0500

    aula web 05072022

--- exemplos.js
+++ exemplos.js
@@ -13,14 +13,28 @@ function calculaArea2(){
 
 function somar(){
     //recuperação de entradas de dados
-    var num1    = document.getElementById("numero1").value;
-    var num2    = document.getElementById("numero2").value;
-    var num3    = document.getElementById("numero3").value;
-    var num4    = document.getElementById("numero4").value;
+    var numero1    = document.getElementById("numero1").value;
+    var numero2    = document.getElementById("numero2").value;
+    var numero3    = document.getElementById("numero3").value;
+    var numero4    = document.getElementById("numero4").value;
     
     //processamento
     var soma = numero1 + numero2 + numero3 + numero4;
 
     //saida
     document.getElementById("soma").value = soma;
+}
+
+function somarAlert(){
+    //recuperação de entradas de dados
+    var numero1    = parseInt( window.prompt("Digite o número 1: ","0") );
+    var numero2    = parseInt( window.prompt("Digite o número 2: ","0") );
+    var numero3    = parseInt( window.prompt("Digite o número 3: ","0") );
+    var numero4    = parseInt( window.prompt("Digite o número 4: ","0") );
+    
+    //processamento
+    var soma = numero1 + numero2 + numero3 + numero4;
+
+    //saida
+    window.alert("A soma é " + soma);
 }
